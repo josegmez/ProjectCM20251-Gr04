f8d3b3fd56c07aa276bd3686b9738ed566ee9e02
daniel-lujan <daniel.lujan@udea.edu.co>
2025-05-14 21:34:40 -0500

    :tada: Initialize project
    Co-authored-by: juanpablo-a <jpablo.arango1@udea.edu.co>
    Co-authored-by: josegmez <jose.gomez14@udea.edu.co>

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.categorias',
+  appName: 'categorias',
+  webDir: 'dist'
+};
+
+export default config;
